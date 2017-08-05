d0507206c3a3d64896638417433f2893de7de019
Mitchell Bartlett <mitchell.bartlett22@gmail.com>
2017-08-05 12:51:07 +1200

    First file

---
+++ GatherBot/Development/AlbionBot/AlbionBot/AlbionBot/Start.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AOKore.Concurrent;
+using AOKore.Game;
+using AOKore.Script;
+
+namespace AlbionBot.AlbionBot
+{
+    class Start
+    {
+    }
+}
