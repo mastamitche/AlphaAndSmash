ecbea542ad60d639da5770716662686774927a03
Mitchell Bartlett <mitchell.bartlett22@gmail.com>
2017-08-08 01:56:41 +1200

    Added new gather bot

--- GatherBot/Development/AlbionBot_AOKore/AlbionBotLibrary/AlbionBot/Program.cs
+++ GatherBot/Development/AlbionBot_AOKore/AlbionBotLibrary/AlbionBot/Program.cs
@@ -9,116 +9,21 @@ using System.Xml.Serialization;
 using AOKore.Concurrent;
 using AOKore.Game;
 using AOKore.Script;
-using System.ComponentModel;
 
-namespace Script
+namespace AlbionBot.AlbionBot
 {
     public class PathData
     {
         public Vector3[] Path { get; set; }
     }
-    public class Configuration
-    {
-        internal double[] WoodHarvestTier = new double[] { };
-        internal double[] RockHarvestTier = new double[] { };
-        internal double[] OreHarvestTier = new double[] { };
-        internal double[] FiberHarvestTier = new double[] { };
-        internal double[] HideHarvestTier = new double[] { };
-        internal double[] RoughDiamondHarvestTier = new double[] { };
 
-        [Category("1. CONFIG")]
-        [DisplayName("Search Range")]
-        public float RangeSearch { get; set; }
-
-        [Category("2. WAYPOINTS")]
-        [DisplayName("(Gather area)")]
-        public string PathWayPoint { get; set; }
-
-        [Category("2. WAYPOINTS")]
-        [DisplayName("(Gather area > City)")]
-        public string PathWayPointCity { get; set; }
-
-        [Category("2. WAYPOINTS")]
-        [DisplayName("(City > Gather area)")]
-        public string PathWayPointHunt { get; set; }
-
-       /* [Category("2. WAYPOINTS")]
-        [DisplayName("(City > Repair)")]
-        public string PathWayPointRepair { get; set; }*/
-
-        //WOOD
-        [Category("3. WOOD")]
-        [DisplayName("Tier")]
-        public string _woodharvestTier
-        {
-            get { return string.Join("; ", WoodHarvestTier.Select(i => i.ToString()).ToArray()); }
-            set { WoodHarvestTier = value.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries).Select(i => double.Parse(i)).ToArray(); }
-        }
-
-        [Category("3. WOOD")]
-        [DisplayName("Collect")]
-        public bool WoodHarvestCondition { get; set; }
-
-        //ROCK
-        [Category("4. ROCK")]
-        [DisplayName("Tier")]
-        public string _rockharvestTier
-        {
-            get { return string.Join("; ", RockHarvestTier.Select(i => i.ToString()).ToArray()); }
-            set { RockHarvestTier = value.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries).Select(i => double.Parse(i)).ToArray(); }
-        }
-
-        [Category("4. ROCK")]
-        [DisplayName("Collect")]
-        public bool RockHarvestCondition { get; set; }
-
-        //ORE
-        [Category("5. ORE")]
-        [DisplayName("Tier")]
-        public string _oreharvestTier
-        {
-            get { return string.Join("; ", OreHarvestTier.Select(i => i.ToString()).ToArray()); }
-            set { OreHarvestTier = value.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries).Select(i => double.Parse(i)).ToArray(); }
-        }
-
-        [Category("5. ORE")]
-        [DisplayName("Collect")]
-        public bool OreHarvestCondition { get; set; }
-
-        //FIBER
-        [Category("6. FIBER")]
-        [DisplayName("Tier")]
-        public string _fiberharvestTier
-        {
-            get { return string.Join("; ", FiberHarvestTier.Select(i => i.ToString()).ToArray()); }
-            set { FiberHarvestTier = value.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries).Select(i => double.Parse(i)).ToArray(); }
-        }
-
-        [Category("6. FIBER")]
-        [DisplayName("Collect")]
-        public bool FiberHarvestCondition { get; set; }
-
-        //HIDE
-        [Category("7. HIDE")]
-        [DisplayName("Tier")]
-        public string _hideharvestTier
-        {
-            get { return string.Join("; ", HideHarvestTier.Select(i => i.ToString()).ToArray()); }
-            set { HideHarvestTier = value.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries).Select(i => double.Parse(i)).ToArray(); }
-        }
-
-        [Category("7. HIDE")]
-        [DisplayName("Collect")]
-        public bool HideHarvestCondition { get; set; }
-
-    }
     public class Program
     {
         [Configuration("Automatic Gathering Settings")]
         public static Configuration Settings { get; set; }
 
         private static ConcurrentList<KeyValuePair<Func<bool>, int>> PlayerData = new ConcurrentList<KeyValuePair<Func<bool>, int>>();
-        public static string path = System.Environment.CurrentDirectory + "\\Plugins\\Cache\\WayPoint\\";
+        public static string path = Environment.CurrentDirectory + "\\Plugins\\Cache\\WayPoint\\";
         public static bool pcity;
         public static bool IsMount = false;
         public static float AtkRanger;
---
+++ GatherBot/Development/AlbionBot_Ennui/V1.4/Albion Gathering Script/GatheringScript.cs
@@ -0,0 +1,135 @@
+﻿using Ennui.Api;
+using Ennui.Api.Direct.Object;
+using Ennui.Api.Meta;
+using Ennui.Api.Method;
+using Ennui.Api.Script;
+using System;
+using System.Collections.Generic;
+
+namespace Ennui.Script.Official
+{
+    [LocalScript]
+    public class GatheringState : StateScript
+    {
+        private Configuration config;
+        private Context context;
+        private Timer timer;
+
+        private void LoadConfig()
+        {
+            try
+            {
+                if (Files.Exists("simple-aio-gatherer.json"))
+                {
+                    config = Codecs.FromJson<Configuration>(Files.ReadAllText("simple-aio-gatherer.json"));
+                    if (config.TypeSetsToUse == null)
+                    {
+                        config.TypeSetsToUse = new List<SafeTypeSet>();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Logging.Log("Failed to log config " + e, LogLevel.Error);
+            }
+
+            if (config == null)
+            {
+                config = new Configuration();
+            }
+        }
+        public override bool OnStart(IScriptEngine se)
+        {
+            LoadConfig();
+
+            context = new Context();
+            timer = new Timer();
+
+            AddHook(() =>
+            {
+				if (RunningKey != "config"
+                    && RunningKey != "login"
+                    && config.AutoRelogin
+                    && (LoginWindow.IsOpen || CharacterSelectWindow.IsOpen || LoginErrorWindow.IsOpen))
+				{
+					Logging.Log("Hooking login");
+					EnterState("login");
+				}
+				return false;
+			});
+
+            AddState("config", new ConfigState(config, context));
+            AddState("resolve", new ResolveState(config, context));
+            AddState("gather", new GatherState(config, context));
+            AddState("combat", new CombatState(config, context));
+            AddState("repair", new RepairState(config, context));
+            AddState("bank", new BankState(config, context));
+			AddState("login", new LoginState(config, context));
+			EnterState("config");
+            return base.OnStart(se);
+        }
+
+        public override void OnPaint(IScriptEngine se, GraphicContext g)
+        {
+            g.SetColor(new Color(0.3f, 0.3f, 0.3f, 1.0f));
+            g.FillRect(15, 100, 265, 210);
+            g.SetColor(new Color(1.0f, 1.0f, 1.0f, 1.0f));
+            g.DrawString("http://ennui.ninja - Simple AIO Gatherer", 20, 100);
+            g.DrawString(string.Format("Runtime: {0}", Time.FormatElapsed(timer.ElapsedMs)), 20, 115);
+            g.DrawString(string.Format("State: {0}", context.State), 20, 130);
+            g.DrawString(string.Format("Max hold weight: {0}", config.MaxHoldWeight), 20, 145);
+            //g.DrawString(string.Format("City cluster: {0}", config.CityClusterName), 20, 160);
+            //g.DrawString(string.Format("Resource cluster: {0}", config.ResourceClusterName), 20, 175);
+			g.DrawString(string.Format("Skip Repairing: {0}", config.skipRepairing), 20, 190);
+			g.DrawString(string.Format("Current Weight: {0}", config.currentWeight), 20, 205);
+			g.DrawString(string.Format("Prioritize Roam Point: {0}", config.roamPointFirst), 20, 220);
+			g.DrawString(string.Format("Repair Waypoints Enabled: {0}", config.enableRepairWayPoints), 20, 235);
+            g.DrawString(string.Format("Bank cluster: {0}", config.VaultClusterName), 20, 160);
+            g.DrawString(string.Format("Repair cluster: {0}", config.RepairClusterName), 20, 175);
+            g.DrawString(string.Format("Resource cluster: {0}", config.ResourceClusterName), 20, 190);
+
+            if (config.ResourceClusterName == Game.ClusterName)
+			{
+				foreach (var p in config.RoamPoints)
+				{
+					p.RealVector3().Expand(3, 3, 3).Render(Api, Color.Red, Color.Red.MoreTransparent());
+				}
+			}
+
+            //if (config.VaultArea != null && config.CityClusterName == Game.ClusterName)
+            if (config.VaultArea != null && config.VaultClusterName == Game.ClusterName)
+            {
+                config.VaultArea.RealArea(Api).Render(Api, Color.Cyan, Color.Cyan.MoreTransparent());
+            }
+
+            //if (config.RepairArea != null && config.CityClusterName == Game.ClusterName)
+            if (config.RepairArea != null && config.RepairClusterName == Game.ClusterName)
+            {
+                config.RepairArea.RealArea(Api).Render(Api, Color.Purple, Color.Purple.MoreTransparent());
+            }
+
+			// MadMonk Extra
+
+			if (config.ExitArea != null && config.RepairClusterName == Game.ClusterName)
+			{
+				config.ExitArea.RealArea(Api).Render(Api, Color.Orange, Color.Orange.MoreTransparent());
+			}
+
+			if (config.RepairWayPointOneArea != null && config.RepairClusterName == Game.ClusterName)
+			{
+				config.RepairWayPointOneArea.RealArea(Api).Render(Api, Color.Black, Color.Black.MoreTransparent());
+			}
+
+			if (config.RepairWayPointTwoArea != null && config.RepairClusterName == Game.ClusterName)
+			{
+				config.RepairWayPointTwoArea.RealArea(Api).Render(Api, Color.Green, Color.Green.MoreTransparent());
+			}
+
+			if (config.RepairWayPointThreeArea != null && config.RepairClusterName == Game.ClusterName)
+			{
+				config.RepairWayPointThreeArea.RealArea(Api).Render(Api, Color.Yellow, Color.Yellow.MoreTransparent());
+			}
+
+		}
+    }
+}
